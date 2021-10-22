b5d3d5ed61cd855dc849fab91ead3a4902d23bb6
cws0316 <dev.chwonseok@gmail.com>
2021-10-22 12:05:21 +0900

    Add: installed Redux

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
+
+let count = 0;
+
+const updateNumber = () => (number.innerText = count);
+
+const handleAdd = () => {
+  count = count + 1;
+  updateNumber();
+};
+
+const handleMinus = () => {
+  count = count - 1;
+  updateNumber();
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);

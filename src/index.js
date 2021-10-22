c77c432dbbdc035d147547ffe89846cc6a411769
cws0316 <dev.chwonseok@gmail.com>
2021-10-22 13:16:34 +0900

    Modify: dispatch and type

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const reducer = () => {};
+const reducer = (state = 0, action) => {
+  if (action.type === 'add') return state + 1;
+  if (action.type === 'minus') return state - 1;
+  return state;
+};
+
 const store = createStore(reducer);
 
+store.dispatch({ type: 'add' });
+store.dispatch({ type: 'add' });
+store.dispatch({ type: 'minus' });
+
+console.log(store.getState());
+
 /************** TAKE AWAY
-createStore는 data를 저장하는 store만드는 function
-reducer는 그 data를 modify하는 function
+createStore는 data(state)를 저장하는 store만드는 function
+reducer는 그 data(state)를 modify하는 function
 
 **************/
 

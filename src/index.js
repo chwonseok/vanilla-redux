2147a27460919d3a9182bafa38b6c22130c9057a
cws0316 <dev.chwonseok@gmail.com>
2021-10-22 12:14:17 +0900

    Add: createStore and reducer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+const reducer = () => {};
+const store = createStore(reducer);
+
+/************** TAKE AWAY
+createStore는 data를 저장하는 store만드는 function
+reducer는 그 data를 modify하는 function
+
+**************/
+
+/* Basic Vanilla JS code
 let count = 0;
 
 const updateNumber = () => (number.innerText = count);
 
 const handleAdd = () => {
-  count = count + 1;
+  count++;
   updateNumber();
 };
 
 const handleMinus = () => {
-  count = count - 1;
+  count--;
   updateNumber();
 };
 
 add.addEventListener('click', handleAdd);
 minus.addEventListener('click', handleMinus);
+*/

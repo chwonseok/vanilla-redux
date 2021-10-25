bdf84bcb7b8d2f2ab0d05ca9ca7a8393fbce5c7a
cws0316 <dev.chwonseok@gmail.com>
2021-10-25 14:14:17 +0900

    Modify: used createReducer from reduxjs/toolkit on StoreJs

--- src/Store.js
+++ src/Store.js
@@ -1,16 +1,19 @@
 import { createStore } from 'redux';
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 export const handleAddTodo = createAction('ADD');
 export const handleDelTodo = createAction('DELETE');
 
-const reducer = (state = [], action) => {
-  if (action.type === handleAddTodo.type)
-    return [...state, { todo: action.payload, id: Date.now() }];
-  if (action.type === handleDelTodo.type)
-    return state.filter((todo) => todo.id !== action.payload);
-  return state;
-};
+// 주의점! createReducer 사용 시, either
+// 1) state를 mutate하거나 (이는 자동적으로 new state로 return이 될 것임 by immer.js)
+// 2) new state를 return 해야 함!
+const reducer = createReducer([], {
+  [handleAddTodo]: (state, action) => {
+    state.push({ todo: action.payload, id: Date.now() });
+  },
+  [handleDelTodo]: (state, action) =>
+    state.filter((todo) => todo.id !== action.payload),
+});
 
 export const store = createStore(reducer);
 
@@ -38,7 +41,7 @@ export const store = createStore(reducer);
 // };
 
 ---------------------------------------------------
-
+* My way not using Date.now() as id in Reducer
 // const ADD = 'ADD';
 // const DELETE = 'DELETE';
 
@@ -55,5 +58,13 @@ export const store = createStore(reducer);
 //     type: DELETE,
 //     id: id,
 //   };
+// };
+
+// const reducer = (state = [], action) => {
+//   if (action.type === handleAddTodo.type)
+//     return [...state, { todo: action.payload, id: action.id }];
+//   if (action.type === handleDelTodo.type)
+//     return state.filter((todo) => todo.id !== action.payload);
+//   return state;
 // };
  * **********************************/
